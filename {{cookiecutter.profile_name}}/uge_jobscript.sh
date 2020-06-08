c1545d980a32c6e82b8c7e033099b1439835757c
Hannah Meyer <hannah.v.meyer@gmail.com>
2020-06-08 07:48:43 -0400

    Remove writing job exit to separate file

--- {{cookiecutter.profile_name}}/uge_jobscript.sh
+++ {{cookiecutter.profile_name}}/uge_jobscript.sh
@@ -17,10 +17,5 @@ qstat -j $JOB_ID | grep '^usage'
 echo "-----------------------------"
 echo "EXIT_STATUS: $EXIT_STATUS"
 
-# save the exit status to file
-CLUSTER_DIR="{{cookiecutter.cluster_dir}}"
-mkdir -p "$CLUSTER_DIR"
-echo "$EXIT_STATUS" >> "$CLUSTER_DIR/$JOB_ID.exit"
-
 # exit with captured exit status
 exit $EXIT_STATUS
